1c6d7007f748e603b0963911c0aa41a47e3d12a6
PRAK Sambath <95472320+PrakSambath@users.noreply.github.com>
2025-10-04 16:52:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,6 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       },
-      base: "/LP-Simulator/"
+      base: '/LP-Simulator/'
     };
 });
